b81120c6202fbcf731df9a07ce0718f60b83ebf1
keremoztopuz <keremoztopuzzz@gmail.com>
2023-07-21 15:32:57 +0300

    server.py : render_template added server.py : parser ip selector added server.py : app.run parameters changed

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template
 import argparse
 
 app = Flask(__name__)
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="server")
     parser.add_argument('--debug', action='store_true', required=False, help='Enable debug mode')
     parser.add_argument('--port', type=int, default=5000, required=False, help='Port number (default: 5000)')
-    args = parser.parse_args()
+    parser.add_argument('--ip', type=str, default='127.0.0.1', required=False, help='IP address (default: 127.0.0.1)')
     args = parser.parse_args()
     print('Hello,', args.debug)
-    app.run(port=args.port,debug=args.debug)
+    app.run(host=args.ip, port=args.port, debug=args.debug)
     
