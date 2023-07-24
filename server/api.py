5779dd5af1b9e284ac1f08a1dfebe477232a5595
Yigit Ugurlu <yigitugurlu2001@gmail.com>
2023-07-24 16:23:49 +0300

    - uploaded request methods - defined a function

--- server/api.py
+++ server/api.py
@@ -1,2 +1,13 @@
-def test_api():
-    return "first project for interns"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/",methods= ["POST","GET","SET","DELETE","PUT"])
+def a_pi():
+    return {"API":"Response Positive"}
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=2000)
+
