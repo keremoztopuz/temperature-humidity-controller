55a4a54db7a231afc3fe66618898be1e8ffefd36
Yigit Ugurlu <yigitugurlu2001@gmail.com>
2023-07-25 15:58:55 +0300

    - api functions added.

--- server/api.py
+++ server/api.py
@@ -1,2 +1,9 @@
 def api_getdata(device_id):
-    return "api_getdata:" +  str(device_id)
+    return "api_getdata:" + " " + str(device_id)
+
+def api_setdata(device_id , temperature):
+    return "api_setdata:" + str(device_id) + " " + str(temperature)
+
+def api_setdevicelist(setdevlist):
+    return "api_setdevicelist:" + " " + str(setdevlist)
+
--- server/server.py
+++ server/server.py
@@ -22,6 +22,16 @@ def click():
 def getdata(device_id : int ):
     return api_getdata(device_id)
 
+@app.route("/api/setdata/<int:device_id>/<float:temperature>", methods=["POST"])
+def setdata(device_id : int , temperature : float):
+    return api_setdata(device_id, temperature)
+
+@app.route("/api/setdevicelist/<setdevlist>", methods=["POST"])
+def setdevicelist(setdevlist: str):
+    return api_setdevicelist(setdevlist)
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="server")
     parser.add_argument('--debug', action='store_true', required=False, help='Enable debug mode')
