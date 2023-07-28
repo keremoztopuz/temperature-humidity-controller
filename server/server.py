d44af1217796bb2a3d006035e6c0c2948c50e4ad
Yiğit Uğurlu <yigitugurlu2001@gmail.com>
2023-07-28 09:11:23 +0300

    -upgraded

--- server/server.py
+++ server/server.py
@@ -1,36 +1,7 @@
 from flask import Flask , render_template
 import argparse
 from api import *
-import psycopg2
-from config import config
 
-def connect():
-    conn = None
-    try:
-        params = config()
-
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-		
-        cur = conn.cursor()
-       
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        db_version = cur.fetchone()
-        print(db_version)
-
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
-if __name__ == '__main__':
-    connect()
 
 
 # main site
